4cfe7613033e4a88bd081a42fdd5f93fec2c2f66
Kyrylo Pylinskyi <k.pylinskyi@gmail.com>
2026-03-28 21:32:12 +0100

    handle board

--- bash/lib/board.sh
+++ bash/lib/board.sh
@@ -1,1 +1,62 @@
 #!/bin/bash
+
+readonly BOARD_SIZE=3
+
+declare -a BOARD
+
+init_board () 
+{
+	BOARD=(" " " " " " " " " " " " " " " " " ")
+}
+
+draw_board () 
+{
+	local top_sep="┌─────┬─────┬─────┐"
+    	local mid_sep="├─────┼─────┼─────┤"
+    	local bot_sep="└─────┴─────┴─────┘"
+
+    	echo "$top_sep"
+	for row in 0 1 2; do
+		local i=$((row * BOARD_SIZE))
+		echo "│  ${BOARD[$i]}  │  ${BOARD[$i+1]}  │  ${BOARD[$i+2]}  │"
+		if [[ $row -lt 2 ]]; then
+			echo "$mid_sep"
+		fi
+	done
+	echo "$bot_sep"
+}
+
+check_win () 
+{
+	:
+}
+
+check_draw () 
+{
+	:
+}
+
+update_board () 
+{
+	local row=$1
+	local col=$2
+	local player=$3
+	local index=$(coords_to_index $row $col)
+	BOARD[$index]=$player
+}
+
+is_cell_empty () 
+{
+	local row=$1
+	local col=$2
+	local index=$(coords_to_index $row $col)
+	[[ "${BOARD[$index]}" == " " ]]
+}
+
+coords_to_index () 
+{
+	local row=$1
+	local col=$2
+	echo $(( (row - 1) * BOARD_SIZE + (col - 1) ))
+}
+
--- bash/main.sh
+++ bash/main.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+source lib/board.sh
+
+init_board
+
+draw_board
