aecd8518548f2f9b8c4384bd676cc981200f7cdc
Kyrylo Pylinskyi <k.pylinskyi@gmail.com>
2026-03-29 15:38:28 +0200

    player: fixes + win/draw check

--- bash/tictactoe/lib/player.sh
+++ bash/tictactoe/lib/player.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
-source board.sh
+. ./board.sh
 
 cursor_row=0
 cursor_col=0
 
 current_player="X"
 
-player_turn ()
-{
-  echo "${current_player}'s turn"
-  player_input
-
-  [[ $current_player="X" ]] && current_player="O" || current_player="X"
-}
-
 player_input ()
 {
   while true; do
     clear
+    echo "${current_player}'s turn"
     draw_board $cursor_row $cursor_col
 
     read -rsn1 key
@@ -27,13 +20,29 @@ player_input ()
       case $key in
         '[A') (( cursor_row > 0 )) && (( cursor_row-- )) ;;
         '[B') (( cursor_row < 2 )) && (( cursor_row++ )) ;;
-        '[C') (( cursor_col > 0 )) && (( cursor_col-- )) ;;
-        '[D') (( cursor_col < 2 )) && (( cursor_col++ )) ;;
+        '[C') (( cursor_col < 2 )) && (( cursor_col++ )) ;;
+        '[D') (( cursor_col > 0 )) && (( cursor_col-- )) ;;
       esac
     elif [[ $key == '' ]]; then
       if is_cell_empty $cursor_row $cursor_col; then
         update_board $cursor_row $cursor_col "$current_player"
-        break 
+        break
       fi
+    fi
   done
 }
+
+player_turn ()
+{
+  player_input
+
+  if check "$current_player"; then
+    echo "Game over. Thanks for playing!"
+    read -rsn1 -p $'Press any key to exit...\n'
+    echo
+    exit 0
+  fi
+
+  [[ $current_player == "X" ]] && current_player="O" || current_player="X"
+}
+
