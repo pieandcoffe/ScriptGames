c28ad2d9026c94f84c66e524c8e5a088c5a7d741
Kyrylo Pylinskyi <k.pylinskyi@gmail.com>
2026-04-27 14:24:13 +0200

    Lua Tetris initial setup

---
+++ lua/tetris/conf.lua
@@ -0,0 +1,6 @@
+function love.conf(t)
+    t.window.title  = "Tetris"
+    t.window.width  = 400
+    t.window.height = 700
+    t.window.vsync  = 1
+end
---
+++ lua/tetris/main.lua
@@ -0,0 +1,21 @@
+-- Every Love2D game starts here
+function love.load()
+
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
+
+function love.keypressed(key)
+    if key == "left"  then
+    elseif key == "right" then
+    elseif key == "up"   then
+    elseif key == "down" then
+    elseif key == "space" then
+    end
+end
