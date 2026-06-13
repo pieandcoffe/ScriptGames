38c73580e2cc83712710bd328cf9e5f1093d8675
Kyrylo Pylinskyi <k.pylinskyi@gmail.com>
2026-06-13 15:00:26 +0200

    feat: scalable windows

--- lua/tetris/conf.lua
+++ lua/tetris/conf.lua
@@ -1,6 +1,9 @@
+SCALE = 4
+local W, H = 240, 160
+
 function love.conf(t)
     t.window.title  = "Tetris"
-    t.window.width  = 400
-    t.window.height = 700
+    t.window.width  = W * SCALE
+    t.window.height = H * SCALE
     t.window.vsync  = 1
 end
--- lua/tetris/main.lua
+++ lua/tetris/main.lua
@@ -1,14 +1,27 @@
--- Every Love2D game starts here
-function love.load()
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
 
+local titleFont
+local uiFont
+
+function love.load()
+    love.graphics.setDefaultFilter("nearest", "nearest")
+    titleFont = love.graphics.newFont(36)
+    uiFont = love.graphics.newFont(18)
+    love.graphics.setFont(titleFont)
 end
 
 function love.update(dt)
-    
 end
 
 function love.draw()
-    
+    love.graphics.push()
+    love.graphics.scale(SCALE, SCALE)
+
+    love.graphics.clear(0.06, 0.12, 0.18)
+
+    love.graphics.pop()
 end
 
 function love.keypressed(key)
@@ -17,5 +30,15 @@ function love.keypressed(key)
     elseif key == "up"   then
     elseif key == "down" then
     elseif key == "space" then
+    end
+end
+
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
     end
 end
