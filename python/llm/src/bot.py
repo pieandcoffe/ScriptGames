348810134d01ea3ee2a52f91d694eb0d4a8c621b
Kyrylo Pylinskyi <k.pylinskyi@gmail.com>
2026-06-15 23:00:44 +0200

    feat: Bot class

--- python/llm/src/bot.py
+++ python/llm/src/bot.py
@@ -1,13 +1,19 @@
 import ollama
-
-from prompt import Prompt
+from src.prompt import Prompt
+from src.history import Message
 
 class Bot:
-    def __init__(self):
-        pass
+    def __init__(self, model: str = "gemma3:4b"):
+        self.model = model
+        self.prompt = Prompt()
 
-    def ask(self, user_input: str):
-        pass
+    def ask(self, user_input: str) -> str:
+        self.prompt.add_conversation_message("user", user_input)
+        messages = self.prompt.build(user_input)
+        
+        response = ollama.chat(model=self.model, messages=messages)
+        reply = response["message"]["content"]
+        
+        self.prompt.add_conversation_message("assistant", reply)
 
-    def reply(self):
-        pass
+        return reply
